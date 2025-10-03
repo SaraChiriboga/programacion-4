2b3d4b236d3990f2e785afa6e7932706cbc1eef4
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2025-10-02 19:16:24 -0500

    S2

---
+++ S2-P4/S2-P4/Controllers/VideojuegoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using S2_P4.Models;
+
+namespace S2_P4.Controllers
+{
+    public class VideojuegoController : Controller
+    {
+        private static List<Videojuego> videojuego = new List<Videojuego>
+        {
+            new Videojuego{Id = 1, Titulo = "FIFA 26", Plataforma = "PS5", Clasificacion = "A"},
+            new Videojuego{Id = 2, Titulo = "MK1", Plataforma = "PS5", Clasificacion = "R"},
+            new Videojuego{Id = 3, Titulo = "Call of Duty WW2", Plataforma = "PS5", Clasificacion = "R"},
+            new Videojuego{Id = 4, Titulo = "Valorant", Plataforma = "PC", Clasificacion = "R"},
+            new Videojuego{Id = 5, Titulo = "Minecraft", Plataforma = "XBOX", Clasificacion = "R"},
+            new Videojuego{Id = 6, Titulo = "Outlast", Plataforma = "PS4", Clasificacion = "R"},
+        };
+        public IActionResult Index()
+        {
+            return View(videojuego);
+        }
+    }
+}
