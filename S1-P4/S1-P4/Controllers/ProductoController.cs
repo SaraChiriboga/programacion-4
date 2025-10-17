eac022ae4af3c31b726bab46c2735b1bcd62355e
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2025-10-16 19:57:52 -0500

    S1

---
+++ S1-P4/S1-P4/Controllers/ProductoController.cs
@@ -0,0 +1,22 @@
+﻿using S1_P4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S1_P4.Controllers
+{
+    public class ProductoController:Controller
+    {
+        public ActionResult Index()
+        {
+            var productos = new List<Producto>
+            {
+                new Producto {Id = 1, Nombre = "Laptop", Precio = 1200},
+                new Producto {Id = 2, Nombre = "Celular", Precio = 1100},
+            };
+            return View(productos);
+        }
+    }
+}
